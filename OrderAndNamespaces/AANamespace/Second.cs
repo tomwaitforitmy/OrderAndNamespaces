18d92d236640f37af5aea1fce0f773c87168f981
Thomas Kilgus <thomas.kilgus@haufe-lexware.com>
2020-04-28 16:54:25 +0200

    Fixed folder structure.

---
+++ OrderAndNamespaces/AANamespace/Second.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using NUnit.Framework;
+
+namespace UnitTestProject1.AANamespace
+{
+    [TestFixture, Order(2)]
+    public class Second
+    {
+        [Test, Order(2), Category("WithNamespace")]
+        public void Order2() //Should run second but runs first
+        {
+            Debug.WriteLine(TestContext.CurrentContext.Test.MethodName);
+        }
+    }
+}
---
+++ OrderAndNamespaces/ZZNamespace/First.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using NUnit.Framework;
+
+namespace UnitTestProject1.ZZNamespace
+{
+    [TestFixture, Order(1)]
+    public class First
+    {
+        [Test, Order(1), Category("WithNamespace")]
+        public void Order1() //Should run first but runs second
+        {
+            Debug.WriteLine(TestContext.CurrentContext.Test.MethodName);
+        }
+    }
+}
